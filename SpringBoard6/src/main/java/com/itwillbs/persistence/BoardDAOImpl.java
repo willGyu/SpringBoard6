746922fd64ef46074163758e4c97003c18e64599
willGyu <jgt8731@itwillbs.co.kr>
2024-10-17 12:42:59 +0900

    read - DAO,mapper처리 완료

--- SpringBoard6/src/main/java/com/itwillbs/persistence/BoardDAOImpl.java
+++ SpringBoard6/src/main/java/com/itwillbs/persistence/BoardDAOImpl.java
@@ -38,6 +38,16 @@ public class BoardDAOImpl implements BoardDAO {
 		logger.debug(" listAll() 호출 ");
 		return sqlSession.selectList(NAMESPACE + "listAll");
 	}
+
+
+	@Override
+	public BoardVO getBoard(int bno) throws Exception {
+		logger.debug(" getBoard(int bno) 호출 ");
+		
+		return sqlSession.selectOne(NAMESPACE + "getBoard",bno);
+	}
+	
+	
 	
 	
 
